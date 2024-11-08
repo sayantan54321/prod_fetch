b52e829a1759d0970ced082e90fdd0314bdb162a
sayantan54321 <sayantan@gikagraph.ai>
2024-11-08 17:44:33 +0530

    Update flask_api.py

--- flask_api.py
+++ flask_api.py
@@ -3,11 +3,11 @@ from dbRetriever.dbRetriever import DBRetriever
 from flask import Flask, request, jsonify
 from typing import List, Dict
 import json
-
+from flask_cors import CORS
 from dbRetriever.outputJsonifier import clean_incomplete_and_trailing_comma
 
 app = Flask(__name__)
-
+CORS(app, origins=["*"]) 
 db_file = 'products_84000.db'
 table_name = 'products'
 retriever = DBRetriever(db_file, table_name)
@@ -26,10 +26,15 @@ def find_products():
     min_match_threshold: int = data.get('min_match_threshold', 1)
     
     try:
-        new_input_pairs, matched_products = retriever.main(input_pairs, min_match_threshold)
+        new_input_pairs, matched_products = retriever.main(input_pairs, len(input_pairs)-1)
         images: List[str] = []
         for i in matched_products:
-            images.extend(products[i[0]]['Image_url'][:2])
+            # images.extend(products[i[0]]['Image_url'][:2])
+            images.append({
+                "url": products[i[0]]['Item_url'][0],
+                "img_src": products[i[0]]['Image_url'][0],
+                "title": products[i[0]]['Display_title'][0]
+            })
         return jsonify({
             'input': new_input_pairs,
             'matched_products': matched_products,
@@ -40,4 +45,4 @@ def find_products():
         return jsonify({'error': str(e)}), 500
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='164.52.194.185', port=5000,debug=True)
